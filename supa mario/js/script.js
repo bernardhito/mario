2f0f039adb35dd410dae5934ec5beb70c9dd5860
rrrrrrrouz <befreitas00@gmail.com>
2024-03-19 13:55:44 -0300

    script.js

--- supa mario/js/script.js
+++ supa mario/js/script.js
@@ -23,7 +23,7 @@ const loop = setInterval(() => {
         mario.style.animation = 'none'; 
         mario.style.bottom = `${marioPosition}px`;
 
-        mario.src = './images/pahblo.jpeg';
+        mario.src = './images/game-over.png';
         mario.style.width = '75px'
         mario.style.marginLeft = '50px'
 
